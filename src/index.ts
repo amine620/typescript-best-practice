54a52581061696a74ce7f632286f590eb77423f9
amine620 <mohafidi1998@gmail.com>
2022-06-15 06:34:39 -1200

    index signature

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,7 @@
-class Account{
-    private id:number;
-    private owner:string;
-    private _balance : number;
-
-    constructor(id:number,owner:string,_balance:number){
-        this.id=id
-        this.owner=owner
-        this._balance=_balance
-    }
-
-    diposit(amount:number):void{
-       if(amount<=0){
-          throw new Error('invalid amount')
-       }
-       this._balance+=amount
-    }
-
-    get balance():number{
-        return this._balance
-    }
-    
-    set balance(value:number){
-        if(value<=0){
-            throw new Error('invalid value')
-         }
-         this._balance=value
-    }
+class setAssignment{
+    [seatNumber:string]:string
 }
+let seat=new setAssignment()
 
-let account=new Account(1,'amine',20)
-
- account.balance
+seat.A1="amine"
+seat.A2="mourid"

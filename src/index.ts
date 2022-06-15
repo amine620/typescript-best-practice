909e9f7e9c9a499c254588cda2dd23c1fb954f36
amine620 <mohafidi1998@gmail.com>
2022-06-15 02:33:15 -1200

    nullish coaelscing operator

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,5 @@
-type Customer={
-    birthday?:Date
+let speed:number|null=null
+
+let ride={
+    speed:speed??30
 }
-
-function getCustomer(id:number):Customer|null|undefined{
-       return id===0?null:{birthday:new Date()}
-}
-let customer=getCustomer(4)
-
-//   optional property access operator
-  console.log( customer?.birthday?.getFullYear());
-
-// optional element access operator
-// customer?.[0]
-
-// optional call
-
-let log:any=null
-log?.('a')
-

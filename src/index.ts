c21158305e3b12659da5712c9af3c94b36be0454
amine620 <mohafidi1998@gmail.com>
2022-06-15 02:29:11 -1200

    optional chanining

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-function greet(name:string|null){
-    if(name)
-      console.log(name.toLowerCase());
-    else
-       console.log('hi');
-       
+type Customer={
+    birthday?:Date
 }
-greet(null)
+
+function getCustomer(id:number):Customer|null|undefined{
+       return id===0?null:{birthday:new Date()}
+}
+let customer=getCustomer(4)
+
+//   optional property access operator
+  console.log( customer?.birthday?.getFullYear());
+
+// optional element access operator
+// customer?.[0]
+
+// optional call
+
+let log:any=null
+log?.('a')
+
